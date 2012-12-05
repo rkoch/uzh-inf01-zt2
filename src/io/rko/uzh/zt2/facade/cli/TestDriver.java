551733e8cb730944b81346fe95e045105ee9f41e
Remo Koch <me@rko.io>
2012-12-05 22:50:33 +0100

    Updated for Yanick

--- src/io/rko/uzh/zt2/facade/cli/TestDriver.java
+++ src/io/rko/uzh/zt2/facade/cli/TestDriver.java
@@ -44,7 +44,7 @@ public class TestDriver {
         }
 
         System.out.println();
-        System.out.println("All done, shutting down.");
+        System.out.println("All done, shutting down."); // Just for you Yanick! :D
         System.exit(0);
     }
 
